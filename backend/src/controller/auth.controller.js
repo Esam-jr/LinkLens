31973fa0449a31a3b43d68f1da01bbff8c5a452a
Esmael <esmaelsabir9@gmail.com>
2025-08-15 10:20:10 +0300

    Add: basic router controller setup and optimizations

---
+++ backend/src/controller/auth.controller.js
@@ -0,0 +1,9 @@
+export async function signup(req, res) {
+  res.send("Signup page");
+}
+export async function login(req, res) {
+  res.send("Login page");
+}
+export function logout(req, res) {
+  res.send("Logout page");
+}
