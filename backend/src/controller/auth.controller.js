01112bd059a0099a0939ce99cda028fbac5d5dd1
Esmael <esmaelsabir9@gmail.com>
2025-08-16 13:49:53 +0300

    Fix User model instantiation and move schema middleware before model creation

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -30,7 +30,7 @@ export async function signup(req, res) {
     const idx = Math.floor(Math.random() * 100) + 1;
     const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
 
-    const newUser = new User({
+    const newUser = await User({
       fullName,
       email,
       password,
