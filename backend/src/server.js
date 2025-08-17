e5662e0585d35e9c0ff309847efaaafdaca8be08
Esmael <esmaelsabir9@gmail.com>
2025-08-17 14:28:41 +0300

    Add chat routes and register user routes in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoute from "./routes/auth.route.js";
-import userRoute from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -14,7 +15,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoute);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
