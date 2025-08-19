0904eb7e1e8683fc19818a11bedc06782b92c2cf
Esmael <esmaelsabir9@gmail.com>
2025-08-19 13:52:48 +0300

    Fix:Add authentication middleware to /me endpoint

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -15,7 +15,7 @@ router.post("/logout", logout);
 
 router.post("/onboarding", protectRoute, onboard);
 
-router.get("/me", (req, res) => {
+router.get("/me", protectRoute, (req, res) => {
   res.status(200).json({ success: true, user: req.user });
 });
 
