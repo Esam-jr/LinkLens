37e72f7c475e03d2af6f509382d604b836431083
Esmael <esmaelsabir9@gmail.com>
2025-08-19 10:35:49 +0300

    Add CORS configuration for frontend development

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
@@ -12,6 +13,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
