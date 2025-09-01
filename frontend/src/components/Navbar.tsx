50921e308e678dfce40d94326674c2f624da818c
Esmael <esmaelsabir9@gmail.com>
2025-09-01 11:35:17 +0100

    Fix logout button functionality and update page title

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -60,7 +60,7 @@ const Navbar = () => {
           {/* Logout button */}
           <button
             className="btn btn-ghost btn-circle"
-            onClick={() => logoutMutation}
+            onClick={() => logoutMutation()}
           >
             <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
           </button>
