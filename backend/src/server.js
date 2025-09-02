04ee84def25c103c207e0cb371b84ad756662197
Esmael <esmaelsabir9@gmail.com>
2025-09-02 13:12:33 +0100

    Add production build configuration and deployment setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,12 +6,15 @@ import chatRoutes from "./routes/chat.route.js";
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -25,6 +28,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
